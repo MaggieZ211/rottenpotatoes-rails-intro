455c517ee628dd102e6f5af8d12528fea5bd1a74
Lulu Zhang <lz2761@columbia.edu>
2022-10-02 02:36:51 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,13 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    if params[:ratings].nil?
+      @ratings_to_show = nil
+    else
+      @ratings_to_show = params[:ratings].keys
+    end
+    
+    @movies = Movie.with_ratings(@ratings_to_show)
   end
 
   def new
